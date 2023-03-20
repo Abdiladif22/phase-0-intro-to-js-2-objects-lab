8ed3da0a83dce8bfde960f1a1f38179dfe449986
Abdiladi22 <abdiladifali0@gmail.com>
2023-03-21 00:09:00 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const employee = {
-    name: "",
-    streetAddress: "",
-  };
-  employee.name = "Abdi";
-  employee.streetAddress = "Muritiina";
-  employee;
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = { ...employee };
-    newEmployee[key] = value;  return newEmployee;
-  }
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee["name"] = "Sam";
-    employee["streetAddress"] = "12 Broadway";
+    "name":"Ali",
+    "streetAddress":"11 Broadway"
+   };
+   function updateEmployeeWithKeyAndValue(employee , key, value) {
+    // let moreInfo = {...object};
+    let newemployee ={...employee}
+    newemployee[key]=value
+    return newemployee
+   }
+   console.log(employee);
+   console.log(updateEmployeeWithKeyAndValue(employee, 'name', 'Sam'));
+   function destructivelyUpdateEmployeeWithKeyAndValue(employee , key, value) {
+   employee[key]=value
+   return employee
+   }
+   // console.log(destructivelyUpdateEmployeeWithKeyAndValue());
+   function deleteFromEmployeeByKey(employee, key) {
+    let anothernewemployee = {...employee}
+    delete anothernewemployee[key]
+    return anothernewemployee;
+   }
+   console.log(deleteFromEmployeeByKey(employee, 'name'));
+   function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
     return employee;
-  }
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee.name;
-    return newEmployee;
-  }
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee["name"];
-    return employee;
-  }
-  destructivelyDeleteFromEmployeeByKey();
+   }
+   console.log(deleteFromEmployeeByKey(employee, 'name'));
